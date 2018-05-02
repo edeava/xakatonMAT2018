02a6316e4e98161432497da1ebe6b7307e5ef981
jelic98 <jelic.ecloga@gmail.com>
2018-05-02 14:50:19 +0200

    Funkcija za ucitavanje

--- Mathak2018/src/main/Functions.java
+++ Mathak2018/src/main/Functions.java
@@ -1,6 +1,49 @@
 package main;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Functions {
+
+    public static char[][] ucitaj(String path) {
+        Scanner s;
+
+        try {
+            s = new Scanner(new File(path));
+        }catch(IOException e) {
+            return null;
+        }
+
+        int r = s.nextInt();
+        int c = s.nextInt();
+
+        char[][] mat = new char[r][c];
+
+        for(int i = 0; i < r; i++) {
+            String line = s.nextLine();
+
+            if(line.length() == 0) {
+                i--;
+                continue;
+            }
+
+            for(int j = 0; j < c; j++) {
+                mat[i][j] = line.charAt(j);
+            }
+        }
+
+        for(int i = 0; i < r; i++) {
+            for(int j = 0; j < c; j++) {
+                System.out.print(mat[i][j]);
+            }
+
+            System.out.println();
+        }
+
+        return mat;
+    }
+
 	public static void ispisiResenje(char mat[][], int n, int m, int rez) {
 		System.out.println(rez);
 		
