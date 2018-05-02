446b06d828f6fe2ddb7d875cb4994a53643a0d93
jelic98 <jelic.ecloga@gmail.com>
2018-05-02 19:57:53 +0200

    Ubacen BFS

--- Mathak2018/src/main/Functions.java
+++ Mathak2018/src/main/Functions.java
@@ -1,7 +1,13 @@
 package main;
 
+import com.sun.javaws.exceptions.InvalidArgumentException;
+import com.sun.tools.corba.se.idl.InvalidArgument;
+
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Scanner;
 
 public class Functions {
@@ -14,7 +20,7 @@ public class Functions {
         try {
             s = new Scanner(new File(path));
         }catch(IOException e) {
-            return null;
+            return new char[0][0];
         }
 
         int r = s.nextInt();
@@ -38,14 +44,15 @@ public class Functions {
         return mat;
     }
 
-	public static void ispisiResenje(char mat[][], int rez) {
+	public static void ispisi(char[][] mat, int rez) {
 		System.out.println(rez);
 
-		for(int i=0;i<mat.length;i++) {
-			for(int j=0;j<mat[i].length;j++) {
+		for(int i = 0; i < mat.length; i++) {
+			for(int j = 0; j < mat[i].length; j++) {
 				System.out.print(mat[i][j]);
 			}
-			System.out.println("");
+
+			System.out.println();
 		}
 	}
 
@@ -86,5 +93,99 @@ public class Functions {
         }
 
         return dist;
+    }
+
+    /**
+     * Format ulazne matrice
+     *
+     * +-+-+-+-+-+-+-+-+-+
+     * |     |X| |X|     |
+     * + + + + + + + + + +
+     * |                X|
+     * + + + + + + + + + +
+     * |X                |
+     * + + + + + + + + + +
+     * |     |X| |X|     |
+     * +-+-+-+-+-+-+-+-+-+
+     *
+     * gde vazi:
+     * X    - cvor u grafu
+     * -    - horizontalna barijera (put ne sme da prodje vertikalno)
+     * |    - vertikalna barijera (put ne sme da prodje horizontalno)
+     * +    - barijera (put ne sme da prodje ni u kom pravcu)
+     */
+    public static Position breadthFirst(char[][] maze, Position source) throws IllegalArgumentException {
+        Queue<Position> queue = new LinkedList<>();
+        queue.offer(source);
+
+        maze[source.row][source.column] = '+';
+
+        while(queue.peek() != null) {
+            Position current = queue.poll();
+
+            ArrayList<Position> neighbors = getNeighbors(maze, current);
+
+            for(Position neighbor : neighbors) {
+                if(maze[neighbor.row][neighbor.column] == 'X') {
+                    return neighbor;
+                }
+
+                queue.offer(neighbor);
+
+                maze[neighbor.row][neighbor.column] = '+';
+            }
+        }
+
+        throw new IllegalArgumentException("Najblizi cvor ne postoji");
+    }
+
+    private static ArrayList<Position> getNeighbors(char[][] maze, Position current) {
+        ArrayList<Position> neighbors = new ArrayList<>();
+
+        // Levo
+        if(current.row > 0
+                && (maze[current.row - 1][current.column] != '+' || maze[current.row - 1][current.column] != '|')) {
+            neighbors.add(new Position(current.row - 1, current.column, current.distance + 1));
+        }
+
+        // Gore
+        if(current.column > 0
+                && (maze[current.row][current.column - 1] != '+' || maze[current.row][current.column - 1] != '-')) {
+            neighbors.add(new Position(current.row, current.column - 1, current.distance + 1));
+        }
+
+        // Desno
+        if(current.row < maze.length - 1
+                && (maze[current.row + 1][current.column] != '+' || maze[current.row + 1][current.column] != '|')) {
+            neighbors.add(new Position(current.row + 1, current.column, current.distance + 1));
+        }
+
+        // Dole
+        if(current.column < maze[0].length - 1
+                && (maze[current.row][current.column + 1] != '+' || maze[current.row][current.column + 1] != '-')) {
+            neighbors.add(new Position(current.row, current.column + 1, current.distance + 1));
+        }
+
+        return neighbors;
+    }
+
+    public static class Position {
+
+        private int row, column, distance;
+
+        public Position(int row, int column, int distance) {
+            this.row = row;
+            this.column = column;
+            this.distance = distance;
+        }
+
+        public int getDistance() {
+            return distance;
+        }
+
+        @Override
+        public String toString() {
+            return "(" + row + ", " + column + ")";
+        }
     }
 }
--- Mathak2018/src/main/Main.java
+++ Mathak2018/src/main/Main.java
@@ -3,6 +3,11 @@ package main;
 public class Main {
 
     public static void main(String[] args) {
+        // TODO ucitati ime fajla preko konzole
         char[][] mat = Functions.ucitaj("test.in");
+
+        Functions.Position closest = Functions.breadthFirst(mat, new Functions.Position(1, 7, 0));
+
+        System.out.println("Najblizi za (1, 7): " + closest.toString() + "\nUdaljenost: " + closest.getDistance());
     }
 }
