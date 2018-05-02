7cbec42cc3888d01035ca7d48dde7ad0493e96af
Buaa <Buaa@LAPTOP-IATJLNI5>
2018-05-02 14:33:45 +0200

    funkcija za ispis resenja

--- Mathak2018/src/main/Functions.java
+++ Mathak2018/src/main/Functions.java
@@ -1,5 +1,14 @@
 package main;
 
 public class Functions {
-
+	public static void ispisiResenje(char mat[][], int n, int m, int rez) {
+		System.out.println(rez);
+		
+		for(int i=0;i<n;i++) {
+			for(int j=0;j<m;j++) {
+				System.out.print(mat[i][j]);
+			}
+			System.out.println("");
+		}
+	}
 }
